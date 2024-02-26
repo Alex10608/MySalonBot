6f0f5d048173c4969fd08c5db8c713a79af87855
Alex Lobov <alexx_lobov@mail.ru>
2024-02-26 15:39:11 +0600

    Фильтры теперь возвращает словари, для работы в хэндлерах.

--- filters/filters.py
+++ filters/filters.py
@@ -6,16 +6,24 @@ from datetime import date
 class CorrectDateFilter(BaseFilter):
     async def __call__(self, message: Message):
         try:
-            d, m, y = message.text.split('.')
+            d, m, y = [int(i) for i in message.text.split('.')]
             is_correct_date = date(year=y, month=m, day=d)
+            return {'birthday': is_correct_date}
         except:
             return False
-        return True
 
 
 class CorrectPhoneFilter(BaseFilter):
     async def __call__(self, message: Message):
-        number = list(filter(lambda x: x.isdigit(), message.text))
+        number = ''.join(filter(lambda x: x.isdigit(), message.text))
+        if len(number) == 10:
+            correct_number = f'8{number}'
+        elif len(number) == 11 and int(number[0]) in (7, 8):
+            correct_number = f'8{number[1:]}'
+        else:
+            return False
+        return {'phone_number': correct_number}
+
         
-        return len(number) == 10 or (len(number) == 11 and number[0] in (7, 8))
+
 
